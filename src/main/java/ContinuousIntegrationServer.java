d2cd7b84d4653ab1f7dc5577c5b2ca4fbb8d65fc
Michaela University Ununtu <michaela.sahlgren@gmail.com>
2021-02-07 14:54:36 +0100

    add: processWebhookCommit method #3
    add method for dealing with webhook request for a commit

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,23 +1,37 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
 import java.io.IOException;
- 
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-/** 
+import java.util.stream.Collectors;
+import org.json.*;
+
+/**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
 */
 public class ContinuousIntegrationServer extends AbstractHandler
 {
+    public static String processWebhookCommit(String requestBody) {
+        JSONObject requestBodyJson = new JSONObject(requestBody);
+        if (requestBodyJson.has("head_commit")) {
+            String headCommitId = requestBodyJson.getJSONObject("head_commit").getString("id");
+            String repoUrl = requestBodyJson.getJSONObject("repository").getString("clone_url");
+            System.out.println("extracted commit and repo: " + headCommitId + " " + repoUrl);
+        }
+
+        return "BUILD SUCCESS";
+    }
+
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
-                       HttpServletResponse response) 
+                       HttpServletResponse response)
         throws IOException, ServletException
     {
         response.setContentType("text/html;charset=utf-8");
@@ -25,6 +39,8 @@ public class ContinuousIntegrationServer extends AbstractHandler
         baseRequest.setHandled(true);
 
         System.out.println(target);
+        String requestBody = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        String webhookCommitResult = processWebhookCommit(requestBody);
 
         // here you do all the continuous integration tasks
         // for example
@@ -33,12 +49,12 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         response.getWriter().println("CI job done");
     }
- 
+
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
-        server.setHandler(new ContinuousIntegrationServer()); 
+        server.setHandler(new ContinuousIntegrationServer());
         server.start();
         server.join();
     }
--- src/test/java/ContinuousIntegrationServerTests.java
+++ src/test/java/ContinuousIntegrationServerTests.java
@@ -1,7 +1,14 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class ContiniousIntegrationServerTests {
+class ContinuousIntegrationServerTests {
+    @Test
+	@DisplayName("Check for build success")
+	void checkBuildSuccessForRepo() {
+        String testRequestBodyJson = "{\"head_commit\": {\"id\": \"7bb8b28ba9d8ae15c063f784ba72ed606a3d344a\"}, \"repository\": {\"clone_url\": \"https://github.com/MichaelaSahlgren/DD2480.git\"}}";
+        String result = ContinuousIntegrationServer.processWebhookCommit(testRequestBodyJson);
+		assertTrue(result.contains("BUILD SUCCESS"));
+	}
 }
