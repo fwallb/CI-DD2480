2c9174ed2498479d31f414b224525bdb0d18c6d5
fridawallberg <frd.wbrg@gmail.com>
2021-02-07 16:04:58 +0100

    add: method to insert data into database #13

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -15,40 +15,17 @@ public class Database {
       conn = DriverManager.getConnection(
         "jdbc:mysql://localhost:3306/history", USER, PWD);
 
-        // Create statement object
-        this.stmt = conn.createStatement();
+      // Create statement object
+      this.stmt = conn.createStatement();
     } catch(SQLException exep) {
       exep.printStackTrace();
     }
   }
 
-  // Method to extract commit history, commit hash and commit date
-  public ArrayList<String[]> getHistory() throws SQLException {
-    ArrayList<String[]> commitList = new ArrayList<String[]>();
-    String[] commit = new String[2];
-
-    String getHistory = "SELECT commitHash, commitDate FROM builds";
-    ResultSet history = this.stmt.executeQuery(getHistory);
-
-    while(history.next()) {
-      commit[0] = history.getString("commitHash");
-      commit[1] = String.valueOf(history.getInt("commitDate"));
-      commitList.add(commit);
-    }
-    return commitList;
-  }
-
-  // Method to extract buildLog based on a commitHash
-  public String getBuildLog(String commitHash) throws SQLException {
-    String result = "No build";
-
-    String getBuildLogQuery = "SELECT buildLog FROM builds WHERE commitHash = " + "'" + commitHash + "'";
-    ResultSet buildLog = this.stmt.executeQuery(getBuildLogQuery);
-
-    while(buildLog.next()) {
-      result = buildLog.getString("buildLog");
-    }
-    return result;
+  // Method to insert a row into database
+  public void insertIntoDatabase(String commitHash, int commitDate, String buildLog) throws SQLException {
+    String insertData = "INSERT INTO builds (commitHash, commitDate, buildLog) VALUES ('" + commitHash + "', '" + commitDate + "', '" + buildLog + "' )";
+    stmt.executeUpdate(insertData);
   }
 
 }
