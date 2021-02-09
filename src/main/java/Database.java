c0eb9c8f7bf6cfd41b510319d99f0ca98b9e0074
fridawallberg <frd.wbrg@gmail.com>
2021-02-09 18:40:21 +0100

    fix: add "this." to instance variables #13

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,28 +4,31 @@ import java.util.Arrays;
 
 public class Database {
 
-  private Statement stmt;
-  private Connection conn;
+  Statement stmt;
+  Connection conn;
   private final String USER = "root";
   private final String PWD = "";
 
   public Database() {
-    try {
-      // Create a connection with a database
-      conn = DriverManager.getConnection(
-        "jdbc:mysql://localhost:3306/history", USER, PWD);
+   try {
+     // Create a connection with a database
+     this.conn = DriverManager.getConnection(
+       "jdbc:mysql://localhost:3306/history", USER, PWD);
 
-      // Create statement object
-      this.stmt = conn.createStatement();
-    } catch(SQLException exep) {
-      exep.printStackTrace();
-    }
+     // Create statement object
+     this.stmt = this.conn.createStatement();
+
+     // Create a new table history if it not exists
+     this.stmt.executeUpdate("CREATE TABLE IF NOT EXISTS builds;");
+   } catch(SQLException exep) {
+     exep.printStackTrace();
+   }
   }
 
   // Method to insert a row into database
   public void insertIntoDatabase(String commitHash, int commitDate, String buildLog) throws SQLException {
     String insertData = "INSERT INTO builds (commitHash, commitDate, buildLog) VALUES ('" + commitHash + "', '" + commitDate + "', '" + buildLog + "' )";
-    stmt.executeUpdate(insertData);
+    this.stmt.executeUpdate(insertData);
   }
 
 }
