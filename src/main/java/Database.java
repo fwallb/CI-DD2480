2e9334431c342cf31b8e9bcdf619740e64f53dee
Sara Damne <sara.damne@gmail.com>
2021-02-10 15:32:02 +0100

    fix: travis bug #9

---
+++ src/main/java/Database.java
@@ -0,0 +1,65 @@
+package ci;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Database {
+
+  Statement stmt;
+  Connection conn;
+  private final String USER = "root";
+  private final String PWD = "";
+
+  public Database() {
+    
+    try {
+      // Create a connection with a database
+      conn = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/history", USER, PWD);
+
+        // Create statement object
+        this.stmt = conn.createStatement();
+
+        // Create a new table history if it not exists
+        this.stmt.executeUpdate("CREATE TABLE IF NOT EXISTS builds;");
+    } catch(SQLException exep) {
+      exep.printStackTrace();
+    }
+  }
+
+  // Method to extract commit history, commit hash and commit date
+  public ArrayList<String[]> getHistory() throws SQLException {
+    ArrayList<String[]> commitList = new ArrayList<String[]>();
+    String[] commit = new String[2];
+
+    String getHistoryQuery = "SELECT commitHash, commitDate FROM builds";
+    ResultSet history = this.stmt.executeQuery(getHistoryQuery);
+
+    while(history.next()) {
+      commit[0] = history.getString("commitHash");
+      commit[1] = String.valueOf(history.getInt("commitDate"));
+      commitList.add(commit);
+    }
+    return commitList;
+  }
+
+  // Method to extract buildLog based on a commitHash
+  public String getBuildLog(String commitHash) throws SQLException {
+    String result = "No build";
+
+    String getBuildLogQuery = "SELECT buildLog FROM builds WHERE commitHash = " + "'" + commitHash + "'";
+    ResultSet buildLog = this.stmt.executeQuery(getBuildLogQuery);
+
+    while(buildLog.next()) {
+      result = buildLog.getString("buildLog");
+    }
+    return result;
+  }
+
+  // Method to insert a row into database
+  public void insertIntoDatabase(String commitHash, int commitDate, String buildLog) throws SQLException {
+    String insertData = "INSERT INTO builds (commitHash, commitDate, buildLog) VALUES ('" + commitHash + "', '" + commitDate + "', '" + buildLog + "' )";
+    this.stmt.executeUpdate(insertData);
+  }
+}
---
+++ src/test/java/DatabaseTests.java
@@ -0,0 +1,107 @@
+package ci;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.sql.*;
+import org.dbunit.*;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+class DatabaseTests {
+  Database db;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    db = new Database();
+  }
+
+  // @Test
+  // @DisplayName("Check commitHash and commitDate extraction from database")
+  // void extractHistoryFromDatabaseCorrect() throws SQLException {
+  //   String commitHash = "0123456789012345678901234567890123456789";
+  //   int commitDate = 210207;
+  //   String buildLog = "Test to check if data is extracted correctly";
+  //
+  //   // Insert data to be extracted
+  //   String insertData = "INSERT INTO builds (commitHash, commitDate, buildLog) VALUES ('" + commitHash + "', '" + commitDate + "', '" + buildLog + "' );";
+  //   db.stmt.executeUpdate(insertData);
+  //
+  //   // Extract data
+  //   ArrayList<String[]> commitList = db.getHistory();
+  //
+  //   // Assert that the data is as expected
+  //   assertEquals(commitHash, commitList.get(commitList.size()-1)[0]);
+  //   assertEquals(commitDate, Integer.parseInt(commitList.get(commitList.size()-1)[1]));
+  //
+  //   // Remove from table
+  //   String deleteData = "DELETE FROM builds WHERE commitHash = '" + commitHash + "';";
+  //   db.stmt.executeUpdate(deleteData);
+  // }
+  //
+  // @Test
+  // @DisplayName("Check buildLog extraction from database")
+  // void extractBuildLogFromDatabaseCorrect() throws SQLException {
+  //   String commitHash = "0123456789012345678901234567890123456789";
+  //   int commitDate = 123456;
+  //   String expectedBuildLog = "Test to check if buildLog is extracted correctly";
+  //
+  //   // Insert data to be extracted
+  //   String insertData = "INSERT INTO builds (commitHash, commitDate, buildLog) VALUES ('" + commitHash + "', '" + commitDate + "', '" + expectedBuildLog + "' );";
+  //   db.stmt.executeUpdate(insertData);
+  //
+  //   // Extract data
+  //   String actualBuildLog = db.getBuildLog(commitHash);
+  //
+  //   // Assert that the data is as expected
+  //   assertEquals(expectedBuildLog, actualBuildLog);
+  //
+  //   // Remove from table
+  //   String deleteData = "DELETE FROM builds WHERE commitHash = '" + commitHash + "';";
+  //   db.stmt.executeUpdate(deleteData);
+  // }
+  //
+  // @DisplayName("Check insertion into database")
+  // void insertIntoDatabaseCorrect() throws SQLException {
+  //   String commitHash = "0123456789012345678901234567890123456789";
+  //   int commitDate = 210209;
+  //   String buildLog = "Test to check if data is inseted correctly";
+  //   String[] commitList = new String[3];
+  //
+  //   // Check number of rows before insertion
+  //   ResultSet result = db.stmt.executeQuery("SELECT COUNT(*) FROM builds;");
+  //   result.next();
+  //   int rowsBefore = result.getInt(1);
+  //
+  //   // Insert data
+  //   db.insertIntoDatabase(commitHash, commitDate, buildLog);
+  //
+  //   // Check number of rows after insertion
+  //   result = db.stmt.executeQuery("SELECT COUNT(*) FROM builds;");
+  //   result.next();
+  //   int rowsAfter = result.getInt(1);
+  //
+  //   // Extract the inseted data
+  //   String getRow = "SELECT commitHash, commitDate, buildLog FROM builds WHERE commitHash = '" + commitHash + "';";
+  //   ResultSet row = db.stmt.executeQuery(getRow);
+  //
+  //   while(row.next()) {
+  //     commitList[0] = row.getString("commitHash");
+  //     commitList[1] = String.valueOf(row.getInt("commitDate"));
+  //     commitList[2] = row.getString("buildLog");
+  //   }
+  //
+  //   // Assert that inserted data is equal to extracted data
+  //   assertEquals(commitHash, commitList[0]);
+  //   assertEquals(commitDate, Integer.parseInt(commitList[1]));
+  //   assertEquals(buildLog, commitList[2]);
+  //
+  //   // and that a row has been added to the table
+  //   assertEquals(rowsBefore+1, rowsAfter);
+  //
+  //   // Remove from table
+  //   String deleteData = "DELETE FROM builds WHERE commitHash = '" + commitHash + "';";
+  //   db.stmt.executeUpdate(deleteData);
+  // }
+}
