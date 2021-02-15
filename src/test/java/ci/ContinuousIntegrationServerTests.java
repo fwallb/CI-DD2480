6c53e64682d76b2181ddf9d753df6398e3a89654
George <george.bassilious@gmail.com>
2021-02-15 02:35:10 +0100

    add: Unit tests for CI-servers #20, add: comments for CI unit-tests #44
    Added two new unit tests that test CI-server methods and added comments for test class.

--- src/test/java/ci/ContinuousIntegrationServerTests.java
+++ src/test/java/ci/ContinuousIntegrationServerTests.java
@@ -1,6 +1,7 @@
 package ci;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.DisplayName;
@@ -33,6 +34,11 @@ class ContinuousIntegrationServerTests {
     HttpServletResponse response;
     JSONObject requestBodyJson;
 
+    /**
+     * Unit tests for CI server:
+     * "known" commits are tested against the methods in ContinuousIntegrationServer class to ensure that they work as they should. 
+     * If a method stops working as it should, these test classes will fail resulting in build fauilure for CI-server.
+    */
     @Test
   	@DisplayName("Check for build success")
   	void checkBuildSuccessForRepo() {
@@ -50,7 +56,44 @@ class ContinuousIntegrationServerTests {
   		assertTrue(result.contains("BUILD SUCCESS"));
   	}
 
-    //Tests for sending email notification
+
+    
+    @Test
+    @DisplayName("Check for build failure when compilation error")
+  	void checkBuildFauilureForCompilationFailure() {
+          String repoUrl = "https://github.com/George-Bassilious/CI-DD2480.git";
+          String commitId = "8ce7f48137f49de6591acc92dd0c7480c27180de";
+          String result = ContinuousIntegrationServer.cloneAndTest(repoUrl, commitId);
+      //for this test to pass both of these statements must be in the commit message
+      //this ensures that the build failed because of compilation errors.
+      assertAll(
+      ()->assertTrue(result.contains("Compilation failure")),
+      ()->assertTrue(result.contains("BUILD FAILURE"))
+      );
+  	
+  	}
+
+    @Test
+    @DisplayName("Check for build failure when junit test fails")
+  	void checkBuildFauilureForUnitTestFailure() {
+          String repoUrl = "https://github.com/George-Bassilious/CI-DD2480.git";
+          String commitId = "6d670e4a1387d9ce3e28643a74bf5113d19007ba";
+          String result = ContinuousIntegrationServer.cloneAndTest(repoUrl, commitId);
+
+      //for this test to pass both of these statements must be in the commit message
+      //this ensures that the build failed because of assertion errors in the test cases.
+      assertAll(
+      ()-> assertTrue(result.contains(".AssertionFailedError")),
+      ()-> assertTrue(result.contains("BUILD FAILURE"))
+      );  
+  		
+  	}
+
+
+
+
+
+    //Unit Tests for methods sending email notification
     @Test
     //If build succeeds
     void sendEmailTestingSuccess(){
